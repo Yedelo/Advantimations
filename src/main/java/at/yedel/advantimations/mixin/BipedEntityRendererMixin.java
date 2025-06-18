2c7f47743dda12e6c805a2fcbcc28876526f55a0
Yedelo <158670400+Yedelo@users.noreply.github.com>
2025-06-18 15:20:14 -0400

    Use @Share to store parameters instead of @Unique fields

--- src/main/java/at/yedel/advantimations/mixin/BipedEntityRendererMixin.java
+++ src/main/java/at/yedel/advantimations/mixin/BipedEntityRendererMixin.java
@@ -4,13 +4,13 @@ package at.yedel.advantimations.mixin;
 
 import at.yedel.advantimations.config.AdvantimationsConfig;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.client.item.ItemModelManager;
 import net.minecraft.client.render.entity.BipedEntityRenderer;
 import net.minecraft.client.render.entity.state.BipedEntityRenderState;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,16 +19,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BipedEntityRenderer.class)
 public abstract class BipedEntityRendererMixin {
-    @Unique
-    private static Entity advantimations$bipedEntity;
-
     @Inject(method = "updateBipedRenderState", at = @At("HEAD"))
-    private static void advantimations$storeBipedEntity(LivingEntity entity, BipedEntityRenderState state, float tickProgress, ItemModelManager itemModelResolver, CallbackInfo ci) {
-        advantimations$bipedEntity = entity;
+    private static void advantimations$storeBipedEntity(LivingEntity entity, BipedEntityRenderState state, float tickProgress, ItemModelManager itemModelResolver, CallbackInfo ci, @Share("entity") LocalRef<LivingEntity> entityRef) {
+        entityRef.set(entity);
     }
 
     @ModifyExpressionValue(method = "updateBipedRenderState", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getHandSwingProgress(F)F"))
-    private static float advantimations$cancelSwings(float original) {
-        return AdvantimationsConfig.getInstance().cancelSwings.getResult(advantimations$bipedEntity, original, 0F);
+    private static float advantimations$cancelSwings(float original, @Share("entity") LocalRef<LivingEntity> entityRef) {
+        return AdvantimationsConfig.getInstance().cancelSwings.getResult(entityRef.get(), original, 0F);
     }
 }
--- src/main/java/at/yedel/advantimations/mixin/UsingItemPropertyMixin.java
+++ src/main/java/at/yedel/advantimations/mixin/UsingItemPropertyMixin.java
@@ -4,12 +4,13 @@ package at.yedel.advantimations.mixin;
 
 import at.yedel.advantimations.config.AdvantimationsConfig;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.client.render.item.property.bool.UsingItemProperty;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.*;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -18,19 +19,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(UsingItemProperty.class)
 public abstract class UsingItemPropertyMixin {
-    @Unique private ItemStack advantimations$stack;
-    @Unique private ItemDisplayContext advantimations$displayContext;
-
     @Inject(method = "test", at = @At("HEAD"))
-    private void advantimations$(ItemStack stack, ClientWorld world, LivingEntity entity, int seed, ItemDisplayContext displayContext, CallbackInfoReturnable<Boolean> cir) {
-        this.advantimations$stack = stack;
-        this.advantimations$displayContext = displayContext;
+    private void advantimations$storePredicateInformation(ItemStack stack, ClientWorld world, LivingEntity entity, int seed, ItemDisplayContext displayContext, CallbackInfoReturnable<Boolean> cir, @Share("stack") LocalRef<ItemStack> stackRef, @Share("displayContext") LocalRef<ItemDisplayContext> displayContextRef) {
+        stackRef.set(stack);
+        displayContextRef.set(displayContext);
     }
 
     @ModifyExpressionValue(method = "test", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;isUsingItem()Z"))
-    private boolean advantimations$cancelItemUseCondition(boolean original) {
-        Item item = advantimations$stack.getItem();
-        if ((advantimations$displayContext == ItemDisplayContext.FIRST_PERSON_LEFT_HAND || advantimations$displayContext == ItemDisplayContext.FIRST_PERSON_RIGHT_HAND) & (AdvantimationsConfig.getInstance().cancelShieldAnimation && item instanceof ShieldItem) || (AdvantimationsConfig.getInstance().cancelArrowAnimation && item instanceof BowItem)) {
+    private boolean advantimations$cancelItemUseCondition(boolean original, @Share("stack") LocalRef<ItemStack> stackRef, @Share("displayContext") LocalRef<ItemDisplayContext> displayContextRef) {
+        Item item = stackRef.get().getItem();
+        ItemDisplayContext displayContext = displayContextRef.get();
+        if ((displayContext == ItemDisplayContext.FIRST_PERSON_LEFT_HAND || displayContext == ItemDisplayContext.FIRST_PERSON_RIGHT_HAND) & (AdvantimationsConfig.getInstance().cancelShieldAnimation && item instanceof ShieldItem) || (AdvantimationsConfig.getInstance().cancelArrowAnimation && item instanceof BowItem)) {
             return false;
         }
         return original;
