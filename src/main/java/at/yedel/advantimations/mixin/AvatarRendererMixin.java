5dfb8daf96807f836c80b8a6eb96fe235c4e8733
Yedelo <158670400+Yedelo@users.noreply.github.com>
2026-02-18 13:54:41 -0500

    Implement cancel elytra animation

--- src/main/java/at/yedel/advantimations/mixin/AvatarRendererMixin.java
+++ src/main/java/at/yedel/advantimations/mixin/AvatarRendererMixin.java
@@ -6,6 +6,7 @@ import at.yedel.advantimations.config.AdvantimationsConfig;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.renderer.entity.player.AvatarRenderer;
+import net.minecraft.client.renderer.entity.state.AvatarRenderState;
 import net.minecraft.world.entity.Avatar;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,6 +16,11 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(AvatarRenderer.class)
 public abstract class AvatarRendererMixin {
+    @ModifyExpressionValue(method = "setupRotations(Lnet/minecraft/client/renderer/entity/state/AvatarRenderState;Lcom/mojang/blaze3d/vertex/PoseStack;FF)V", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/entity/state/AvatarRenderState;isFallFlying:Z", opcode = Opcodes.GETFIELD))
+    private boolean advantimations$cancelElytraAnimation(boolean original, @Local(argsOnly = true) AvatarRenderState state) {
+        return AdvantimationsConfig.getInstance().cancelElytraAnimation.getThirdPersonResult(state, original, false);
+    }
+
     @ModifyExpressionValue(method = "getArmPose(Lnet/minecraft/world/entity/Avatar;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/client/model/HumanoidModel$ArmPose;", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/Avatar;swinging:Z", opcode = Opcodes.GETFIELD))
     private static boolean advantimations$cancelSwings(boolean original, @Local(argsOnly = true) Avatar avatar) {
         return AdvantimationsConfig.getInstance().cancelSwings.getThirdPersonResult(avatar.asLivingEntity(), original, false);
