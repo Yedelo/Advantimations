9d4311ae03dbe07ea93963ffd9d695549c74c002
Yedelo <158670400+Yedelo@users.noreply.github.com>
2026-02-18 17:21:29 -0500

    merge modifications of render state into one method

--- src/main/java/at/yedel/advantimations/mixin/HumanoidMobRendererMixin.java
+++ src/main/java/at/yedel/advantimations/mixin/HumanoidMobRendererMixin.java
@@ -3,24 +3,22 @@ package at.yedel.advantimations.mixin;
 
 
 import at.yedel.advantimations.config.AdvantimationsConfig;
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
+import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
+import net.minecraft.client.renderer.item.ItemModelResolver;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
 
 @Mixin(HumanoidMobRenderer.class)
 public abstract class HumanoidMobRendererMixin {
-    @ModifyExpressionValue(method = "extractHumanoidRenderState", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getAttackAnim(F)F"))
-    private static float advantimations$cancelSwings(float original, @Local(argsOnly = true) LivingEntity entity) {
-        return AdvantimationsConfig.getInstance().cancelSwings.getThirdPersonResult(entity, original, 0F);
-    }
-
-    @ModifyExpressionValue(method = "extractHumanoidRenderState", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isCrouching()Z"))
-    private static boolean advantimations$cancelSneaking(boolean original, @Local(argsOnly = true) LivingEntity entity) {
-        return AdvantimationsConfig.getInstance().cancelSneaking.getThirdPersonResult(entity, original, false);
+    @Inject(method = "extractHumanoidRenderState", at = @At(value = "RETURN"))
+    private static void advantimations$modifyHumanoidRenderState(LivingEntity entity, HumanoidRenderState state, float f, ItemModelResolver itemModelResolver, CallbackInfo ci) {
+        state.attackTime = AdvantimationsConfig.getInstance().cancelSwings.getThirdPersonResult(entity, state.attackTime, 0F);
+        state.isCrouching = AdvantimationsConfig.getInstance().cancelSneaking.getThirdPersonResult(entity, state.isCrouching, false);
     }
 }
--- src/main/java/at/yedel/advantimations/mixin/LivingEntityRendererMixin.java
+++ src/main/java/at/yedel/advantimations/mixin/LivingEntityRendererMixin.java
@@ -3,29 +3,24 @@ package at.yedel.advantimations.mixin;
 
 
 import at.yedel.advantimations.config.AdvantimationsConfig;
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
 
 @Mixin(LivingEntityRenderer.class)
 public abstract class LivingEntityRendererMixin {
-    @ModifyExpressionValue(method = "extractRenderState(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isAutoSpinAttack()Z"))
-    private boolean advantimations$cancelRiptideAnimation(boolean original, @Local(argsOnly = true) LivingEntity entity) {
-        return AdvantimationsConfig.getInstance().cancelRiptideAnimation.getThirdPersonResult(entity, original, false);
+    @Inject(method = "extractRenderState(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;F)V", at = @At("RETURN"))
+    private void advantimations$modifyLivingEntityRenderState(LivingEntity entity, LivingEntityRenderState state, float f, CallbackInfo ci) {
+        state.isAutoSpinAttack = AdvantimationsConfig.getInstance().cancelRiptideAnimation.getThirdPersonResult(entity, state.isAutoSpinAttack, false);
+        state.walkAnimationSpeed = AdvantimationsConfig.getInstance().cancelLimbMovements.getThirdPersonResult(entity, state.walkAnimationSpeed, 0F);
+        state.walkAnimationPos = AdvantimationsConfig.getInstance().weirderLimbMovements.getThirdPersonResult(entity, state.walkAnimationPos, 0F);
     }
 
-    @ModifyExpressionValue(method = "extractRenderState(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/WalkAnimationState;speed(F)F"))
-    private float advantimations$cancelLimbMovements(float original, @Local(argsOnly = true) LivingEntity entity) {
-        return AdvantimationsConfig.getInstance().cancelLimbMovements.getThirdPersonResult(entity, original, 0F);
-    }
 
-    @ModifyExpressionValue(method = "extractRenderState(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/WalkAnimationState;position(F)F"))
-    private float advantimations$weirderLimbMovements(float original, @Local(argsOnly = true) LivingEntity entity) {
-        return AdvantimationsConfig.getInstance().weirderLimbMovements.getThirdPersonResult(entity, original, 0F);
-    }
 }
