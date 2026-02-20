0b19931ea982b027ce8d9cc13b081581db14eac2
Yedelo <158670400+Yedelo@users.noreply.github.com>
2026-02-19 20:27:49 -0500

    Use @ModifyExpressionValue to target entity render state

--- src/main/java/at/yedel/advantimations/mixin/LevelRendererMixin.java
+++ src/main/java/at/yedel/advantimations/mixin/LevelRendererMixin.java
@@ -2,25 +2,21 @@ package at.yedel.advantimations.mixin;
 
 
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.client.Camera;
-import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.client.renderer.entity.state.EntityRenderState;
-import net.minecraft.client.renderer.state.LevelRenderState;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
 
 @Mixin(LevelRenderer.class)
 public abstract class LevelRendererMixin {
-    @Inject(method = "extractVisibleEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/LevelRenderer;extractEntity(Lnet/minecraft/world/entity/Entity;F)Lnet/minecraft/client/renderer/entity/state/EntityRenderState;", shift = At.Shift.BY, by = 2))
-    private void advantimations$appendToState(Camera camera, Frustum frustum, DeltaTracker deltaTracker, LevelRenderState levelRenderState, CallbackInfo ci, @Local Entity entity, @Local EntityRenderState state) {
+    @ModifyExpressionValue(method = "extractVisibleEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/LevelRenderer;extractEntity(Lnet/minecraft/world/entity/Entity;F)Lnet/minecraft/client/renderer/entity/state/EntityRenderState;"))
+    private EntityRenderState advantimations$appendToState(EntityRenderState state, @Local Entity entity) {
         state.advantimations$updateId(entity);
+        return state;
     }
 }
