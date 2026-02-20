71d17fbbb398471dbf131a542117d98d3c15460a
Yedelo <158670400+Yedelo@users.noreply.github.com>
2026-02-20 00:12:19 -0500

    Fix cancel spyglass animation targetting an invalid method in PlayerItemInHandLayer

--- src/main/java/at/yedel/advantimations/mixin/PlayerItemInHandLayerMixin.java
+++ src/main/java/at/yedel/advantimations/mixin/PlayerItemInHandLayerMixin.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(PlayerItemInHandLayer.class)
 public abstract class PlayerItemInHandLayerMixin {
-    @ModifyExpressionValue(method = "submitArmWithItem(Lnet/minecraft/client/renderer/entity/state/AvatarRenderState;Lnet/minecraft/client/renderer/item/ItemStackRenderState;Lnet/minecraft/world/entity/HumanoidArm;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;I)V", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/entity/state/AvatarRenderState;isUsingItem:Z", opcode = Opcodes.GETFIELD))
+    @ModifyExpressionValue(method = "submitArmWithItem(Lnet/minecraft/client/renderer/entity/state/AvatarRenderState;Lnet/minecraft/client/renderer/item/ItemStackRenderState;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/HumanoidArm;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;I)V", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/entity/state/AvatarRenderState;isUsingItem:Z", opcode = Opcodes.GETFIELD))
     private boolean advantimations$cancelSpyglassAnimation(boolean original, @Local(argsOnly = true) AvatarRenderState state) {
         return AdvantimationsConfig.getInstance().cancelSpyglassAnimation.getThirdPersonResult(state, original, false);
     }
