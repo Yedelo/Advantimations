1d2c1319ee9feab923739e2b937a3688d59ba220
Yedelo <158670400+Yedelo@users.noreply.github.com>
2026-04-07 19:33:38 -0400

    Add class tweaker namespace replacement to stonecutter.gradle.kts

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -9,5 +9,9 @@ stonecutter parameters {
         string(current.parsed >= "1.21.11") {
             replace("ResourceLocation", "Identifier")
         }
+
+        string(current.parsed >= "26.1") {
+            replace("classTweaker v1 named", "classTweaker v1 official")
+        }
     }
 }
