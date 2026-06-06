a127d336c038e5680b400f2684be584c554c4382
Yedelo <158670400+Yedelo@users.noreply.github.com>
2026-06-06 15:42:48 -0400

    Update stonecutter 0.9.1-beta.4 -> 0.9.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
 }
 
 plugins {
-	id("dev.kikugie.stonecutter") version "0.9.1-beta.4"
+	id("dev.kikugie.stonecutter") version "0.9.5"
 	id("dev.kikugie.loom-back-compat") version "0.2"
 }
 
