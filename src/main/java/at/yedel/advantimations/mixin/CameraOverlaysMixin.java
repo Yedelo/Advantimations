f50ba0af332054f9cc68c00e11efda032b2f38d3
Yedelo <158670400+yedelo@users.noreply.github.com>
2026-06-23 00:52:56 -0400

    Fix cancel spyglass animation for 26.2, rename it to be general

--- src/main/java/at/yedel/advantimations/mixin/CameraOverlaysMixin.java
+++ src/main/java/at/yedel/advantimations/mixin/CameraOverlaysMixin.java
@@ -4,16 +4,22 @@ package at.yedel.advantimations.mixin;
 
 import at.yedel.advantimations.config.AdvantimationsConfig;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import net.minecraft.client.gui.Gui;
+/*? >= 26.2 {*/
+import net.minecraft.client.gui.Hud;
+import org.spongepowered.asm.mixin.injection.At;
+/*?} else {*/
+/*import net.minecraft.client.gui.Gui;*/
+/*?}*/
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 
 
-@Mixin(Gui.class)
-public abstract class GuiMixin {
+@Mixin(/*? >= 26.2 {*/ Hud.class /*?} else {*//*Gui.class*//*?}*/)
+public abstract class CameraOverlaysMixin {
     @ModifyExpressionValue(method = /*? >= 26.1 {*/"extractCameraOverlays"/*?} else {*//*"renderCameraOverlays"*//*?}*/, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;isScoping()Z"))
     private boolean advantimations$cancelSpyglassAnimation(boolean original) {
         return AdvantimationsConfig.getInstance().cancelSpyglassAnimation.getFirstPersonResult(original, false);
     }
+
 }
