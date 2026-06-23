a39aec9713bcdb3c79df5fc6ce078bb8e3dd3a1a
Yedelo <158670400+yedelo@users.noreply.github.com>
2026-06-23 00:52:54 -0400

    Add version 26.2 to stonecutter

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ loomx.loomVersion = "1.16-SNAPSHOT"
 
 stonecutter {
 	create(rootProject) {
-		versions("1.21.4", "1.21.9", "1.21.11", "26.1")
+		versions("1.21.4", "1.21.9", "1.21.11", "26.1", "26.2")
 		vcsVersion = "26.1"
 	}
 }
