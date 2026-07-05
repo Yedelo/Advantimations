e1e7be027a748fb6fd255a92e88512cd6b95553f
Yedelo <158670400+yedelo@users.noreply.github.com>
2026-07-05 15:10:29 -0400

    Fix settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,13 +11,22 @@ pluginManagement {
 plugins {
 	id("dev.kikugie.stonecutter") version "0.9.5"
 	id("dev.kikugie.loom-back-compat") version "0.2"
+	id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 loomx.loomVersion = "1.16-SNAPSHOT"
 
 stonecutter {
 	create(rootProject) {
-		versions("1.21.4", "1.21.9", "1.21.11", "26.1", "26.2")
-		vcsVersion = "26.1"
+		fun registerProject(versionString: String, loader: String) {
+			version("$versionString-$loader", versionString).buildscript("build.$loader.gradle.kts")
+		}
+
+		for (versionString in listOf("1.21.4", "1.21.9", "1.21.11", "26.1", "26.2")) {
+			registerProject(versionString, "fabric")
+			registerProject(versionString, "neoforge")
+		}
+		registerProject("1.21.8", "neoforge")
+		vcsVersion = "26.2-fabric"
 	}
 }
